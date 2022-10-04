86e54b126013c18570057311359fe740dc6127ff
nutanlondhe <nutanlondhe85930@gmail.com>
2022-10-04 21:31:29 +0530

    console method
    console method

---
+++ demo.js
@@ -0,0 +1,6 @@
+console.log("My name is Nutan Londhe");
+console.log("My Hobbies : 1.Dancing 2.Travelling 3.Reading");
+console.info("My Strength: Possitive Attitude, Self Motivated , Disciplined, Honest");
+console.warn("My Weakness: Easily trust to anyone , emotional ")
+console.log(30+50);
+console.log(49*479);
