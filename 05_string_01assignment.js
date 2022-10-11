0c2f72095b10ac52c3eaf3dd9c5e7a26b39ad5f7
nutanlondhe <nutanlondhe85930@gmail.com>
2022-10-12 01:16:10 +0530

    string
    string

---
+++ 05_string_01assignment.js
@@ -0,0 +1,15 @@
+console.log("-----------My Dream Company Name--------------");
+console.log("My Dream Company is Mindspace");
+
+console.log("-----------My Hobbies-----------");
+var hobby_1 = " Dancing";
+var hobby_2 = " Watching Movie";
+var hobby_3 = " Listing Music";
+var hobby_4 = " Reading Books";
+console.log("Hobby-1=" + hobby_1);
+console.log("Hobby-2=" + hobby_2);
+console.log("Hobby-3=" + hobby_3);
+console.log("Hobby-4=" + hobby_4);
+
+console.log("................concat string ..................");
+console.log( hobby_1+", "+hobby_2+", "+hobby_3+", "+hobby_4);
